d9e6443ba7e68d99c6eb31e83348ac8b2df9c25c
Filipinski <140195559+filipinski1@users.noreply.github.com>
2024-03-20 12:44:20 -0400

    Add project files.

---
+++ Data/TrelloWeb1Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrelloWeb1.Models;
+
+namespace TrelloWeb1.Data
+{
+    public class TrelloWeb1Context : DbContext
+    {
+        public TrelloWeb1Context (DbContextOptions<TrelloWeb1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TrelloWeb1.Models.Customer> Customer { get; set; } = default!;
+
+        public DbSet<TrelloWeb1.Models.Projects>? Projects { get; set; }
+
+        public DbSet<TrelloWeb1.Models.Employee>? Employee { get; set; }
+    }
+}
